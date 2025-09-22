91d49e2426f0dbf03afa06e1def0076e1234e91e
ramil <rm.tj.777@gmail.com>
2025-09-22 18:20:09 +0300

    init proj

---
+++ src/main/java/ru/itmo/isitmolab/HelloApplication.java
@@ -0,0 +1,9 @@
+package ru.itmo.isitmolab;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
