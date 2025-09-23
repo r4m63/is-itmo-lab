2552f2142dd3acde2d9dd4516be60587144a92b6
ramil <rm.tj.777@gmail.com>
2025-09-24 00:09:06 +0300

    chore(backend): implement hibernate api

--- src/main/java/ru/itmo/isitmolab/model/Vehicle.java
+++ src/main/java/ru/itmo/isitmolab/model/Vehicle.java
@@ -6,14 +6,18 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "vehicle")
 public class Vehicle {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // генерирует БД
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
@@ -36,7 +40,7 @@ public class Vehicle {
     @Column(nullable = false)
     private VehicleType type;
 
-    @Positive // null допустим, >0 если задан
+    @Positive
     private Integer enginePower;
 
     @Min(1)
@@ -63,93 +67,5 @@ public class Vehicle {
         if (creationDate == null) creationDate = new Date();
     }
 
-    // getters/setters ...
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Coordinates getCoordinates() {
-        return coordinates;
-    }
-
-    public void setCoordinates(Coordinates coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public VehicleType getType() {
-        return type;
-    }
-
-    public void setType(VehicleType type) {
-        this.type = type;
-    }
-
-    public Integer getEnginePower() {
-        return enginePower;
-    }
-
-    public void setEnginePower(Integer enginePower) {
-        this.enginePower = enginePower;
-    }
-
-    public int getNumberOfWheels() {
-        return numberOfWheels;
-    }
-
-    public void setNumberOfWheels(int numberOfWheels) {
-        this.numberOfWheels = numberOfWheels;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public Integer getDistanceTravelled() {
-        return distanceTravelled;
-    }
-
-    public void setDistanceTravelled(Integer distanceTravelled) {
-        this.distanceTravelled = distanceTravelled;
-    }
-
-    public float getFuelConsumption() {
-        return fuelConsumption;
-    }
-
-    public void setFuelConsumption(float fuelConsumption) {
-        this.fuelConsumption = fuelConsumption;
-    }
-
-    public FuelType getFuelType() {
-        return fuelType;
-    }
-
-    public void setFuelType(FuelType fuelType) {
-        this.fuelType = fuelType;
-    }
 }
