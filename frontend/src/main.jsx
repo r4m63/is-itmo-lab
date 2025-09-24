01008c6c14345d3ed5b684da7ccbc10e57d48fef
ramil <rm.tj.777@gmail.com>
2025-09-24 16:58:22 +0300

    chore(frontend): startup react app + heroui
    - startup app template
    - implement heroui lib with any dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {HeroUIProvider} from "@heroui/react";
+import {Toaster} from "sonner";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <HeroUIProvider>
+          <Toaster expand={false} richColors/>
+          <App/>
+      </HeroUIProvider>
   </StrictMode>,
 )
