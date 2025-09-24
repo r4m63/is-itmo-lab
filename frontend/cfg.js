5013f70e7421fbd5b1ca9644002836ca5518a31b
ramil <rm.tj.777@gmail.com>
2025-09-24 21:08:36 +0300

    feat(frontend): basic authorization skeleton
    - app.jsx -- access rights, public & private routes
    - zustand auth store
    - cfg.js with basic route to backend
    - basic pages plug

---
+++ frontend/cfg.js
@@ -0,0 +1,2 @@
+export const API_BASE = "http://localhost:8000";
+export const FRONTEND_BASE = "http://localhost:3000";
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,82 @@
+import React, {useEffect} from "react";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import LoginPage from "./page/loginPage.jsx";
+import MainPage from "./page/mainPage.jsx";
+import NotFoundPage from "./page/notFoundPage.jsx";
+import {toast} from "sonner";
+import useAuthStore from "./store/auth.js";
+import {API_BASE} from "../cfg.js";
+
 export default function App() {
 
+    const {isAuthed, setIsAuthed} = useAuthStore();
+
+    const PrivateRoute = ({children}) => {
+        if (isAuthed === null) return null;
+        return isAuthed ? children : <Navigate to="/login" replace/>;
+        // toast.error('Сессия не найдена.');
+    };
+
+    const PublicRoute = ({children}) => {
+        if (isAuthed === null) return null;
+        return isAuthed ? <Navigate to="/" replace/> : children;
+    };
+
+    const checkSession = async () => {
+        try {
+            const res = await fetch(`${API_BASE}/api/auth/check-session`, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (res.ok) {
+                setIsAuthed(true);
+            } else {
+                switch (res.status) {
+                    case 401:
+                        setIsAuthed(false);
+                        // toast.error('Сессия не найдена.');
+                        break;
+                    default:
+                        setIsAuthed(false);
+                        toast.error(`Ошибка сервера: ${res.status} ${res.statusText}`);
+                        break;
+                }
+            }
+        } catch (err) {
+            setIsAuthed(false);
+            toast.error("Не удалось подключиться к серверу.");
+        }
+    };
+
+    useEffect(() => {
+        checkSession();
+    }, [setIsAuthed]);
+
     return (
-        <>
-            <h1>Hello</h1>
-        </>
-    )
+        <Router>
+            <Routes>
+                {/* PUBLIC */}
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage/>
+                    </PublicRoute>
+                }/>
+
+                {/* PRIVATE */}
+                <Route path="/" element={
+                    <PrivateRoute>
+                        <MainPage/>
+                    </PrivateRoute>
+                }/>
+
+                {/* 404 */}
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
