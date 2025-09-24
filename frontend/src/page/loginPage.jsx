11ea37fef0a74e727f366a2e7ac7cb4f987db03a
ramil <rm.tj.777@gmail.com>
2025-09-24 21:14:49 +0300

    feat(frontend): login page

--- frontend/src/page/loginPage.jsx
+++ frontend/src/page/loginPage.jsx
@@ -1,7 +1,73 @@
+// src/page/LoginPage.jsx
+
+import {useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import {Button, Input} from "@heroui/react";
+import {toast} from "sonner";
+import useAuthStore from "../store/auth.js";
+import style from "./loginPage.module.css";
+import {API_BASE, FRONTEND_BASE} from "../../cfg.js";
+
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {setIsAuthed} = useAuthStore();
+    const navigate = useNavigate();
+    const from = useLocation().state?.from?.pathname || "/";
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch(`${API_BASE}/api/auth/login`, {
+                method: "POST",
+                credentials: "include",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email, password}),
+            });
+
+            if (res.ok) {
+                setIsAuthed(true);
+                navigate(from, {replace: true});
+                toast.success("Успешный вход!");
+                return;
+            }
+
+            if (res.status === 401) {
+                toast.error("Неверный логин или пароль!");
+            } else {
+                toast.error(`Ошибка сервера: ${res.status} ${res.statusText}`);
+            }
+        } catch (err) {
+            toast.error("Не удалось подключиться к серверу.");
+        }
+    };
+
     return (
-        <>
-            LoginPage
-        </>
+        <div className={style.loginWrapper}>
+            <form className={style.loginForm} onSubmit={handleSubmit}>
+                <h1 className={style.loginTitle}>Войдите в свой аккаунт</h1>
+
+                <Input
+                    label="Логин"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+
+                <Input
+                    label="Пароль"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <Button type="submit" color="primary" fullWidth>
+                    Войти
+                </Button>
+            </form>
+        </div>
     );
 }
